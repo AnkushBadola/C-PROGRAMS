ee2679c7a15c34ef5dc71d25949bf216c435a390
ANKUSH BADOLA <Ankushbadola1@gmail.com>
2026-01-16 20:52:07 +0530

    Add files via upload
    Step 1: Header Inclusion
    The program begins with #include <stdio.h>. This command tells the compiler to include the Standard Input Output library. Without this, the program would not understand what printf (output) or scanf (input) are.
    
    Step 2: Entering the Main Function
    Execution starts at int main(). This is the entry point of every C program. The int indicates that the program will return an integer value to the operating system upon completion (usually 0 for success).
    
    Step 3: Memory Reservation (Variable Declaration)
    Inside the curly braces, the line int a, b, c, d, sum; allocates space in the computer's RAM.
    
    It creates four integer slots for your inputs (a, b, c, d).
    
    It creates one slot for the result (sum).
    
    Step 4: The Interaction Cycle (Prompt & Capture)
    The program then enters a repetitive four-step process to gather data:
    
    Prompt: printf displays a message on the screen asking for a specific number.
    
    Capture: scanf("%d", &a); pauses execution. The %d tells the computer to wait for an integer, and the &a tells it to store that integer in the specific memory address reserved for a.
    
    This cycle repeats for variables b, c, and d.
    
    Step 5: The Processing Phase (Calculation)
    Once the variables are filled with user data, the CPU performs the arithmetic: sum = a + b + c + d;.
    
    The computer fetches the values from the four memory addresses.
    
    It adds them together using the ALU (Arithmetic Logic Unit).
    
    it stores the final result in the memory address assigned to the sum variable.
    
    Step 6: Final Output Formatting
    The line printf("the sum of %d, %d, %d, %d is %d\n", a, b, c, d, sum); performs a "string interpolation."
    
    The program replaces each %d in the text with the actual values stored in the variables, in the order they are listed at the end of the line.
    
    The \n moves the cursor to a new line for a clean terminal look.
    
    Step 7: Program Termination
    Finally, return 0; signals to the operating system that the program has finished its task successfully and closes the process.

---
+++ SUM OF 4 NUMBERS.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int a,b,c,d,sum;
+printf("enter first number :");
+scanf("%d", &a);
+printf("enter second number :");
+scanf("%d", &b);
+printf("enter third number :");
+scanf("%d", &c);
+printf("enter fourth number :");
+scanf("%d", &d);
+sum=a+b+c+d;
+printf("the sum of %d,%d,%d,%d is %d\n",a,b,c,d,sum);
+return 0;
+}
